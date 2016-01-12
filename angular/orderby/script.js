6343cf0b62dd7ca8ea3c5eb5923dc4ff8f86a887
Lucker Xia <lucker.xia@missionsky.com>
2016-01-12 17:39:00 +0800

    angular order

---
+++ angular/orderby/script.js
@@ -0,0 +1,73 @@
+(function(angular) {
+  'use strict';
+angular.module('exampleApp', []).
+  controller('EventController', ['$scope', function($scope) {
+   
+    $scope.list = [
+        {
+          "order": 1,
+          "name": "index",
+          "children": [
+            {
+              "order": null,
+              "children": []
+            }
+          ]
+        },
+        {
+          "order": 9,
+          "name": "about",
+          "children": [
+            {
+              "order": null,
+              "children": []
+            }
+          ]
+        },
+        {
+          "order": 2,
+          "name": "solution",
+          "children": [
+            {
+              "order": null,
+              "children": []
+            }
+          ]
+        },
+        {
+          "order": 11,
+          "name": "module",
+          "children": [
+            {
+              "order": null,
+              "children": []
+            }
+          ]
+        },
+        {
+          "order": 100,
+          "name": "help",
+          "children": [
+            {
+              "order": null,
+              "children": []
+            }
+          ]
+        }
+      ];
+
+
+    /*
+     * expose the event object to the scope
+     */
+    $scope.clickMe = function() {
+      $scope.entropy = new Date().getTime();
+    };
+    
+
+
+   
+
+    
+  }]);
+})(window.angular);
