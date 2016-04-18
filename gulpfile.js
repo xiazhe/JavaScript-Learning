e76e4f36b9d678a9ef265e0bc70f6a1399cfaf4f
Zhe Xia <xiazhe@outlook.com>
2016-04-18 12:40:21 +0800

    updated DOM event

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,43 @@
 /**
  * Created by zhexia on 16/4/13.
  */
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+// webserver
+gulp.task('webserver', function () {
+    gulp.src('./')
+        .pipe(plugins.webserver({
+            liveload: true,
+            directoryListing: true,
+            open: true,
+            host: 'localhost',
+            port: 9000,
+            fallback: 'index.html'
+        }));
+        
+    // gulp.start('watch');
+});
+
+// watch
+gulp.task('watch', function(){
+   gulp.watch('**/*.html');
+   gulp.watch('**/*.js');
+   gulp.watch('**/*.css');
+   plugins.livereload.listen();
+    
+   gulp.watch('**/*.html')
+       .on('change', plugins.livereload.changed)
+       .on('error', errorHandler);
+});
+
+function errorHandler(error){
+    console.log(error.toString());
+    this.emit('end');
+};
+
+
+// Bower
+gulp.task('bower', function(){
+    return plugins.bower({ cmd: 'update'});
+});
